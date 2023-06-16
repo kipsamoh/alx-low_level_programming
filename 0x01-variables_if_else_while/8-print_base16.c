fbe6cada720760d0e74862d9150f2b8f1df96117
kipsamoh <kipsamoh@gmail.com>
2023-06-16 11:11:14 -0700

    rectified swap values

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main(void)
 {
 	int b;
 
-	for (b = 97; b < 103; b++)
+	for (b = 48; b < 58; b++)
 	{
 		putchar(b);
 	}
-	for (b = 48; b < 58; b++)
+	for (b = 97; b < 103; b++)
 	{
 		putchar(b);
 	}
