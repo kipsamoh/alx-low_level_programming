c16f47a28a83844f75ba55adeb037b71f2e03927
kipsamoh <kipsamoh@gmail.com>
2023-06-20 14:53:00 -0700

    Fibonacci sequence whose values do not exceed 4,000,000,

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - outputs the sum of even valued fibonacci nums.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	long int b1, b2, cb, dcb;
+
+	b1 = 1;
+	b2 = 2;
+	cb = dcb = 0;
+	while (fn <= 4000000)
+	{
+		cb = b1 + b2;
+		b1 = b2;
+		b2 = cb;
+		if ((b1 % 2) == 0)
+		{
+			dcb += b1;
+		}
+	}
+	printf("%ld\n", dcb);
+	return (0);
+}
