84bd672bf1cea07ecd4ad0a3f7667a1f54dfa7c6
kipsamoh <kipsamoh@gmail.com>
2023-07-11 00:35:09 -0700

    100-argstostr.c

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,52 +1,40 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * argstostr - concatenates arguments
- * @ac: counter of arguments
- * @av: argument array
- *
- * Return: array of char pointer
+ * argstostr - concatenates programs
+ * @ac: counter input
+ * @av: array vector
+ * Return: 0
  */
 char *argstostr(int ac, char **av)
 {
-	char *chararray;
-	int a, b, c, d;
+	int a, b, c = 0, d = 0;
+	char *cararray;
 
-	if (ac == 0)
+	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	for (a = b = 0; b < ac; b++)
+	for (a = 0; a < ac; a++)
 	{
-		if (av[b] == NULL)
-			return (NULL);
-
-		for (c = 0; av[b][c] != '\0'; c++)
-			b++;
-		c++;
-	}
-
-	chararray = malloc((a + 1) * sizeof(char));
-
-	if (chararray == NULL)
-	{
-		free(chararray);
-		return (NULL);
-	}
-
-	for (b = c = d = 0; d < a; b++, d++)
-	{
-		if (av[b][c] == '\0')
-		{
-			chararray[d] = '\n';
-			b++;
+		for (b = 0; av[a][b]; b++)
 			d++;
-			c = 0;
-		}
-		if (d < a - 1)
-			chararray[d] = av[b][c];
 	}
-	chararray[d] = '\0';
+	d += ac;
 
-	return (chararray);
+	cararray = malloc(sizeof(char) * d + 1);
+	if (cararray == NULL)
+		return (NULL);
+	for (a = 0; a < ac; a++)
+	{
+	for (b = 0; av[a][b]; b++)
+	{
+		cararray[c] = av[a][b];
+		d++;
+	}
+	if (cararray[c] == '\0')
+	{
+		cararray[c++] = '\n';
+	}
+	}
+	return (cararray);
 }
