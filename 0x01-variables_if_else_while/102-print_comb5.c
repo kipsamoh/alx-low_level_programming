d69a8e6194a91a10bc38a083ad6fcfa67472f124
kipsamoh <kipsamoh@gmail.com>
2023-07-12 12:41:41 -0700

    update

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
-/**
- * main - ouputs nums from 00-99
- *
- * Return: 0 (successful)
- */
 
-int main() 
-{
+int main() {
     int i = 0;
-
     while (i < 100) {
         int j = i + 1;
         while (j < 100) {
@@ -16,22 +9,23 @@ int main()
             int y = i % 10;
             int z = j / 10;
             int w = j % 10;
+
             putchar(x + '0');
             putchar(y + '0');
             putchar(' ');
             putchar(z + '0');
             putchar(w + '0');
-	    if (i != 98 || j != 99) 
-	    {
-		    break;
-	    }
+
+            if (!(i == 98 && j == 99)) {
                 putchar(',');
                 putchar(' ');
             }
+
             j++;
         }
         i++;
     }
 
+    putchar('\n');
     return 0;
 }
