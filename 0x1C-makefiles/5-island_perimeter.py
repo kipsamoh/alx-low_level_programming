2cef9ea4dfaf30a3814288e6972842706c4ec548
kipsamoh <133492659+kipsamoh@users.noreply.github.com>
2023-11-22 19:07:17 +0300

    5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,19 +3,19 @@
 
 
 def island_perimeter(grid):
-    """returns the perimeter of the island described in grid.
+    """returns the _perimeter of the is_land descri_bed in grid.
 
     Args:
         grid (list): grid
     """
-    m, n = len(grid), len(grid[0])
-    land, neighbour = 0, 0
-    for i in range(m):
+    _m, n = len(grid), len(grid[0])
+    land, _neighbour = 0, 0
+    for i in range(_m):
         for j in range(n):
             if grid[i][j] == 1:
                 land += 1
-                if i < m - 1 and grid[i+1][j] == 1:
-                    neighbour += 1
+                if i < _m - 1 and grid[i+1][j] == 1:
+                    _neighbour += 1
                 if j < n - 1 and grid[i][j + 1] == 1:
-                    neighbour += 1
-    return land * 4 - 2 * neighbour
+                    _neighbour += 1
+    return land * 4 - 2 * _neighbour
